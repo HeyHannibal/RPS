407e939380b31abf1f9dcb9ba3eb8dfde9c461ff
Hannibal <danilaivanov.iv@gmail.com>
2021-09-01 22:26:02 +0300

    RPS

--- RPS.js
+++ RPS.js
@@ -1,79 +1,162 @@
-
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function(e) => {
-  alert("Hello World");
-});
-
-
-
-FUCK GIT 
-
-
-/*
-
-
-let RPS = [ 
+let RPS = [
     "rock",
-    "paper", 
+    "paper",
     "scissors"
 ]
 
 
 
 
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = "rock";
+    playRound();
+    document.querySelector(".playerImage").src = "Icons/rock.png";
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = "paper";
+    playRound();
+    document.querySelector(".playerImage").src = "Icons/paper.png";
+
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerSelection = "scissors";
+    playRound();
+    document.querySelector(".playerImage").src = "Icons/scissors.png";
+
+});
+
+const playerScore = document.querySelector('#PS');
+const match = document.querySelector('#match');
+const computerScore = document.querySelector('#CP');
 
 
 function computerPlay() {
-    return RPS[Math.floor(Math.random()*RPS.length)];
+    let rollthedice = RPS[Math.floor(Math.random() * RPS.length)];
+    document.querySelector(".computerImage").src = ("Icons/" + rollthedice.toString() + ".png")
+    return rollthedice;
+
 }
 
-let PS = 0; 
-let CP = 0; 
-function playRound() {
-    const computerSelection = computerPlay(); 
-    
-    const playerSelection =  computerPlay();  //prompt("choose your weapon").toLowerCase(); 
-    
-    console.log(playerSelection, computerSelection)
-    
-    
-  
-    if (playerSelection == computerSelection) {
-        return ("it\'s a Tie.")
-        }    
-    else if ((playerSelection + computerSelection === "rockscissors")   
-            || (playerSelection + computerSelection === "paperrock") 
-            || (playerSelection + computerSelection === "scissorspaper")) {
-        PS = PS + 1; 
-        return  ("You Won! " + playerSelection + " beats " + computerSelection + ".") ;       
-        }
-    else { 
-        CP = CP + 1; 
-        return ("You lose. " + computerSelection + " beats " + playerSelection + ".")
-        }
-    
+
+function toRoman(num) {
+    let i = "I"
+    let v = "V"
+    if (num <= 3) {
+        return i.repeat(num)
     }
-  
-function game() {
-    let i = 5; 
+    else if (num == 4)
+        return i.concat(v)
+    else if (num == 5) {
+        return v;
+    }
+    else {
+        return "i'll make more numbers later"
+    }
+}
+
+
+let CP = 0;
+let PS = 0;
+
+
+
+function playRound() {
+
+    const computerSelection = computerPlay();
+
+
+
+
+    if (playerSelection == computerSelection) {
+        match.textContent = ("it\'s a Tie.");
+    }
+    else if ((playerSelection + computerSelection === "rockscissors")
+        || (playerSelection + computerSelection === "paperrock")
+        || (playerSelection + computerSelection === "scissorspaper")) {
+        PS = PS + 1;
+        playerScore.textContent = toRoman(PS)
+        match.textContent = ("You Won! " + playerSelection + " beats " + computerSelection + ".");
+        if (PS == 5) {
+            match.textContent = ("YOU HAVE WON. FOR NOW");
+            playerScore.textContent = toRoman(PS);
+            computerScore.textContent = toRoman(CP);
+            PS = 0;
+            CP = 0;
+            
+        }
+
+
+    }
+    else {
+        CP = CP + 1;
+        computerScore.textContent = toRoman(CP);
+        match.textContent = ("You lose. " + computerSelection + " beats " + playerSelection + ".")
+        if (CP == 5) {
+            match.textContent = ("THIS IS THE END OF YOU");
+            playerScore.textContent = toRoman(PS);
+            computerScore.textContent = toRoman(CP);
+            PS = 0;
+            CP = 0;
+            
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/* function game() {
+    let i = 5;
     while (CP < i || PS < i ) {
-        console.log(playRound()) 
-        if (CP === i || PS === i) { break; } 
+        console.log(playRound())
+        if (CP === i || PS === i) { break; }
         console.log("Your score: " + PS + ". " + "Computer Score: " + CP + ".")
 
     }
-    
+
     console.log("Your score: " + PS + ". " + "Computer Score: " + CP + ".")
-   
+
     if (PS > CP) {
         console.log("Flawless Victory")
-        
+
     }
     else if (CP > PS) {
         console.log("Another Humiliating Defeat. How Pathetic.")
     }
-    return CP = 0, PS = 0; 
-    
+    return CP = 0, PS = 0;
+
 }
- */
+    function endgame() {
+            if (CP == 5) {
+                match.textContent = ("THIS IS THE END OF YOU");
+                PS = 0;
+                CP = 0;
+                playerScore.textContent = "Your Score: " + PS;
+                computerScore.textContent = "Computer Score: " + CP;
+            }
+            else if (PS == 5) {
+                match.textContent = ("YOU HAVE WON. FOR NOW");
+                PS = 0;
+                CP = 0;
+                playerScore.textContent = "Your Score: " + PS;
+                computerScore.textContent = "Computer Score: " + CP;
+            }
+            else {continue;}
+}
+
+*/
