738021171127b5576872610b7eed9e92ba9e0ca0
Hannibal <danilaivanov.iv@gmail.com>
2021-09-05 19:36:52 +0300

    changed style, game functions and  code formatting

--- RPS.js
+++ RPS.js
@@ -1,12 +1,3 @@
-let RPS = [
-    "rock",
-    "paper",
-    "scissors"
-]
-
-
-
-
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     playerSelection = "rock";
@@ -34,43 +25,25 @@ const playerScore = document.querySelector('#PS');
 const match = document.querySelector('#match');
 const computerScore = document.querySelector('#CP');
 
+let RPS = [
+    "rock",
+    "paper",
+    "scissors"
+]
 
-function computerPlay() {
+let CS = 0;
+let PS = 0;
+
+function computerPlay() { 
     let rollthedice = RPS[Math.floor(Math.random() * RPS.length)];
     document.querySelector(".computerImage").src = ("Icons/" + rollthedice.toString() + ".png")
     return rollthedice;
 
 }
 
-
-function toRoman(num) {
-    let i = "I"
-    let v = "V"
-    if (num <= 3) {
-        return i.repeat(num)
-    }
-    else if (num == 4)
-        return i.concat(v)
-    else if (num == 5) {
-        return v;
-    }
-    else {
-        return "i'll make more numbers later"
-    }
-}
-
-
-let CP = 0;
-let PS = 0;
-
-
-
-function playRound() {
+function playRound() { 
 
     const computerSelection = computerPlay();
-
-
-
 
     if (playerSelection == computerSelection) {
         match.textContent = ("it\'s a Tie.");
@@ -79,84 +52,38 @@ function playRound() {
         || (playerSelection + computerSelection === "paperrock")
         || (playerSelection + computerSelection === "scissorspaper")) {
         PS = PS + 1;
-        playerScore.textContent = toRoman(PS)
-        match.textContent = ("You Won! " + playerSelection + " beats " + computerSelection + ".");
-        if (PS == 5) {
-            match.textContent = ("YOU HAVE WON. FOR NOW");
-            playerScore.textContent = toRoman(PS);
-            computerScore.textContent = toRoman(CP);
-            PS = 0;
-            CP = 0;
-            
+        playerScore.textContent = PS;
+        match.textContent = ("You've Won! " + playerSelection + beatsOrBeat(playerSelection) + computerSelection + ".")
+        whoWon()    
         }
-
-
-    }
     else {
-        CP = CP + 1;
-        computerScore.textContent = toRoman(CP);
-        match.textContent = ("You lose. " + computerSelection + " beats " + playerSelection + ".")
-        if (CP == 5) {
-            match.textContent = ("THIS IS THE END OF YOU");
-            playerScore.textContent = toRoman(PS);
-            computerScore.textContent = toRoman(CP);
-            PS = 0;
-            CP = 0;
-            
+        CS = CS + 1;
+        computerScore.textContent = CS;
+        
+        match.textContent = ("You lose. " + computerSelection + beatsOrBeat(computerSelection) + playerSelection + ".")
+        whoWon()  
         }
     }
 
-
-
+function whoWon() {
+    if  (PS == 5) {
+        match.textContent = ("You Have Won");
+        PS = 0;
+        CS = 0;
+        playerScore.textContent = PS;
+        computerScore.textContent = CS;} 
+    else if (CS == 5) {
+        match.textContent = ("You Died");
+        PS = 0;
+        CS = 0;
+        playerScore.textContent = PS;
+        computerScore.textContent = CS;      
 }
+} 
+        
 
-
-
-
-
-
-
-
-
-
-
-/* function game() {
-    let i = 5;
-    while (CP < i || PS < i ) {
-        console.log(playRound())
-        if (CP === i || PS === i) { break; }
-        console.log("Your score: " + PS + ". " + "Computer Score: " + CP + ".")
-
+function beatsOrBeat(computerSelection, playerSelection) {
+    if (computerSelection === "scissors" || playerSelection === "scissors") {
+        return " beat "}
+    else {return " beats "}
     }
-
-    console.log("Your score: " + PS + ". " + "Computer Score: " + CP + ".")
-
-    if (PS > CP) {
-        console.log("Flawless Victory")
-
-    }
-    else if (CP > PS) {
-        console.log("Another Humiliating Defeat. How Pathetic.")
-    }
-    return CP = 0, PS = 0;
-
-}
-    function endgame() {
-            if (CP == 5) {
-                match.textContent = ("THIS IS THE END OF YOU");
-                PS = 0;
-                CP = 0;
-                playerScore.textContent = "Your Score: " + PS;
-                computerScore.textContent = "Computer Score: " + CP;
-            }
-            else if (PS == 5) {
-                match.textContent = ("YOU HAVE WON. FOR NOW");
-                PS = 0;
-                CP = 0;
-                playerScore.textContent = "Your Score: " + PS;
-                computerScore.textContent = "Computer Score: " + CP;
-            }
-            else {continue;}
-}
-
-*/
